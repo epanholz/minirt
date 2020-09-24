9aaecebbf9bda30a2cf537694db4d22b1afd4cf8
Zinobias <66080544+Zinobias@users.noreply.github.com>
2020-09-24 14:02:25 +0200

    UwU

---
+++ utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   color_utils.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pani_zino <pani_zino@student.codam.nl>       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/22 17:38:28 by pani_zino     #+#    #+#                 */
+/*   Updated: 2020/09/24 13:59:02 by pani_zino     ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+int		rgbt(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+void	my_mlx_pixel_put(t_minirt *minirt, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = minirt->var.addr + (y * minirt->var.line_length + x
+		* (minirt->var.bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	ft_delay(void)
+{
+	int	c;
+	int	d;
+
+	c = 0;
+	d = 0;
+	while (c <= 238609293)
+	{
+		while (d <= 238609293)
+			d++;
+		c++;
+	}
+}
+
+void	ft_save(void)
+{
+	write(1, "\nsᴀᴠɪɴɢ ʙɪᴛᴍᴀᴘ ", 33);
+	ft_delay();
+	write(1, ". ", 3);
+	ft_delay();
+	write(1, ". ", 3);
+	ft_delay();
+	write(1, ". \n\n", 5);
+	ft_delay();
+}
